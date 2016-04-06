39044b66d31b39725ed88d0cf00f57aad43c7009
mitchpowell1 <mdp9648@uncw.edu>
2016-04-06 01:28:31 -0400

    Began tracking .sql files

---
+++ insert_clco_data.sql
@@ -0,0 +1,581 @@
+insert into Building values
+
+	('FSC','Fisher Student Center');
+
+insert into Building values
+
+	('BU','Burney Center');
+
+insert into Building values
+
+	('WC', 'Warwick Center');
+
+insert into Building values
+
+	('FUU', 'Fisher University Union');
+
+
+
+insert into StoreKey values
+
+	('A2A', 'A2A Key');
+
+insert into StoreKey values
+
+	('A1', 'A1 Key');
+
+
+
+insert into Item values
+
+	(001, 'Chair', 'Four Legs, a seat and a back. The Chair you know and love');
+
+insert into Item values
+
+	(002, 'Skinny 6 Foot Table', 'Skinny 6 foot table, like a catering table, but skinny');
+
+insert into Item values
+
+	(003, 'Table Skirt', 'An ornate piece of fabric preventing the viewing of the knobby knees of those sitting at the table');
+
+insert into Item values
+
+	(004, 'Crate', 'Holds milk, table skirts, whatever you need');
+
+insert into Item values
+
+	(005, 'Handicap Sign', 'A small sign to be hung on the back of a chair, designating it\'s use for the handicapped');
+
+insert into Item values
+
+	(006, 'Chair Cart', 'A cart for hauling around chairs. Up to 10 at a time.');
+
+insert into Item values
+
+	(007, 'Cocktail Table', 'A small, round table');
+
+insert into Item values
+
+	(008, 'Round Table', 'A large round table');
+
+insert into Item values
+
+	(009, 'Catering Table', 'A large rectangular table, approximately 6 feet in length and three feet in width.');
+
+insert into Item values
+
+	(010, 'Table Cart', 'A means to haul around catering tables and six foot skinny tables');
+
+insert into Item values
+
+	(011, 'Flag', 'US or NC Flag');
+
+insert into Item values
+
+	(013, 'Projector Stand', 'A small square table with detachable legs to hold a projector.');
+
+insert into Item values
+
+	(014, 'Easel', 'Tall, three legged holder of posters');
+
+insert into Item values
+
+	(015, 'Portable Projector Screen', 'Like an Easel with a million-pound screen attached. A real treat');
+
+insert into Item values
+
+	(016, 'Silver Stanchion', 'A short silver pole with a ribbon that comes out of the top to keep a crowd in line.');
+
+insert into Item values
+
+	(017, 'Black Stanchion', 'A short black pole with a ribbon that comes out of the top to keep a crowd in line.');
+
+insert into Item values
+
+	(018, 'Wheelchair', 'Like a regular chair but with a couple of wheels instead of legs');
+
+insert into Item values
+
+	(019, 'Umbrella', 'Protects your delicate skin from harmful UV rays of the sun');
+
+insert into Item values
+
+	(020, 'Freedom of Expression Sign','Signs to be put out when there are events in the ampitheatre');
+
+insert into Item values
+
+	(021, 'Ladder', 'Two long metal bars connected by several smaller, perpendicular metal bars');
+
+insert into Item values
+
+	(022, 'Masonboro Chair', 'A special chair of the finest upholstry for use in the Masonboro Island room');
+
+insert into Item values
+
+	(023, 'Closed for Event Sign', 'A pole with a sign on top bearing the enscription: "Closed For Event"');
+
+insert into Item values
+
+	(024, 'Lecturn', 'A large stand upon which a speaker may place notes and into which a microphone may be plugged');
+
+insert into Item values
+
+	(025, 'Podium', 'A smaller, simple stand on which a speaker may place notes');
+
+insert into Item values
+
+	(027, 'Partition Handle', 'A small handle to be placed in a hole in the wall and turned until the partition is able to be opened or is fully closed');
+
+insert into Item values
+
+	(028, 'Roll of Gaff Tape', 'A long, thin, strip of some sort of plastic with an adhesive on one side.');
+
+insert into Item values
+
+	(029, 'XLR Cable (Medium)', 'A cable with three pins used to connect audio equipment. Medium Length');
+
+insert into Item values
+
+	(030, 'XLR Cable (Long)', 'A cable with three pins used to connect audio equipment. Long Length');
+
+insert into Item values
+
+	(031, 'XLR Cable (Short)', 'A cable with three pins used to connect audio equipment. Short Length');
+
+insert into Item values
+
+	(032, 'VGA Cable', 'Three connected cables used to connect video/audio equipment to a soundboard');
+
+insert into Item values
+
+	(034, '1/8" to 1/4" Interface Cable', 'A cable with a 1/8" jack on one end and a 1/4" jack on the other');
+
+insert into Item values
+
+	(035, '1/4" Cable', 'A cable with a 1/4" jack on both ends');
+
+insert into Item values
+
+	(036, 'Power Cable', 'Provides power');
+
+insert into Item values
+
+	(037, 'Mic Stand', 'A stand that holds a microphone');
+
+insert into Item values
+
+	(038, 'Wireless Advancer', 'Changes slides in presentations, looks like a remote');
+
+insert into Item values
+
+	(039, 'DI Box', null);
+
+insert into Item values
+
+	(040, 'Tech Laptop', 'A computer that folds up and can sit on your lap');
+
+insert into Item values
+
+	(041, 'Microphone', 'Mic Check 1 2');
+
+insert into Item values
+
+	(042, '3-pin DMX Cable', NULL);
+
+insert into Item values
+
+	(043, '5-pin DMX Cable', NULL);
+
+insert into Item values
+
+	(044, 'Projector Remote', 'Controls a projector remotely');
+
+insert into Item values
+
+	(045, '1/4" to XLR Cable', 'Audio cable that converts from 1/4" to XLR');
+
+insert into Item values
+
+	(046, 'Small Snake', NULL);
+
+
+
+insert into Storage values
+
+	(57001, 'FUU', '1041', 'Long Leaf Pine Storage', 'A2A');
+
+insert into Storage values
+
+	(57002, 'BU', '1017', 'Burney Storage', 'A2A');
+
+insert into Storage values
+
+	(57003, 'WC', '113', 'Warwick Ballroom Storage', 'A1');
+
+insert into Storage values
+
+	(57004, 'FSC', '1006B', 'The Dungeon', 'A2A');
+
+insert into Storage values
+
+	(57005, 'FSC', '2002', 'Clocktower Storage', 'A2A');
+
+insert into Storage values
+
+	(57006, 'FSC', '2036', 'Fishbowl Storage', 'A2A');
+
+insert into Storage values
+
+	(57007, 'FSC', '2006', 'Masonboro Storage', 'A2A');
+
+insert into Storage values
+
+	(57008, 'FUU', '2016', 'Azalea Storage', 'A2A');
+
+insert into Storage values
+
+	(57009, 'FUU', '2025', 'Union Tech Storage', 'A1');
+
+insert into Storage values
+
+	(57010, 'BU', '1003', 'Burney Tech Storage', 'A1');
+
+
+
+
+
+insert into Stored values
+
+	(1, 57001, 001, 50);
+
+insert into Stored values
+
+	(2, 57001, 002, 10);
+
+insert into Stored values
+
+	(3, 57001, 003, 5);
+
+insert into Stored values
+
+	(4, 57001, 004, 1);
+
+insert into Stored values
+
+	(5, 57001, 005, 1);
+
+insert into Stored values
+
+	(6, 57001, 006, 1);
+
+insert into Stored values
+
+	(7, 57002, 001, 600);
+
+insert into Stored values
+
+	(8, 57002, 007, 20);
+
+insert into Stored values
+
+	(9, 57002, 008, 55);
+
+insert into Stored values
+
+	(10, 57002, 009, 65);
+
+insert into Stored values
+
+	(11, 57002, 02, 5);
+
+insert into Stored values
+
+	(12, 57002, 011, 3);
+
+insert into Stored values
+
+	(13, 57002, 011, 3);
+
+insert into Stored values
+
+	(14, 57003, 009, 39);
+
+insert into Stored values
+
+	(15, 57003, 02, 8);
+
+insert into Stored values
+
+	(16, 57003, 001, 183);
+
+insert into Stored values
+
+	(17, 57003, 003, 35);
+
+insert into Stored values
+
+	(18, 57003, 013, 1);
+
+insert into Stored values
+
+	(19, 57003, 014, 5);
+
+insert into Stored values
+
+	(20, 57003, 015, 3);
+
+insert into Stored values
+
+	(21, 57004, 009, 12);
+
+insert into Stored values
+
+	(22, 57004, 003, 10);
+
+insert into Stored values
+
+	(23, 57004, 016, 12);
+
+insert into Stored values
+
+	(24, 57004, 017, 21);
+
+insert into Stored values
+
+	(25, 57004, 018, 2);
+
+insert into Stored values
+
+	(26, 57004, 011, 2);
+
+insert into Stored values
+
+	(27, 57004,011, 2);
+
+insert into Stored values
+
+	(28, 57004, 001, 50);
+
+insert into Stored values
+
+	(29, 57004, 007, 1);
+
+insert into Stored values
+
+	(30, 57004, 019, 15);
+
+insert into Stored values
+
+	(31, 57004, 020, 3);
+
+insert into Stored values
+
+	(32, 57004, 014, 2);
+
+insert into Stored values
+
+	(33, 57004, 021, 1);
+
+insert into Stored values
+
+	(34, 57005, 022, 55);
+
+insert into Stored values
+
+	(35, 57005, 009, 5);
+
+insert into Stored values
+
+	(36, 57005, 023, 1);
+
+insert into Stored values
+
+	(37, 57005, 004, 1);
+
+insert into Stored values
+
+	(38, 57005, 003, 2);
+
+insert into Stored values
+
+	(39, 57006, 024, 1);
+
+insert into Stored values
+
+	(40, 57006, 025, 1);
+
+insert into Stored values
+
+	(41, 57006, 011, 2);
+
+insert into Stored values
+
+	(42, 57006, 004, 1);
+
+insert into Stored values
+
+	(43, 57006, 007, 4);
+
+insert into Stored values
+
+	(44, 57006, 015, 2);
+
+insert into Stored values
+
+	(45, 57007, 008, 6);
+
+insert into Stored values
+
+	(46, 57007, 02, 15);
+
+insert into Stored values
+
+	(47, 57007, 010, 1);
+
+insert into Stored values
+
+	(48, 57008, 002, 18);
+
+insert into Stored values
+
+	(49, 57008, 003, 10);
+
+insert into Stored values
+
+	(50, 57008, 001, 120);
+
+insert into Stored values
+
+	(51, 57008, 008, 8);
+
+insert into Stored values
+
+	(52, 57008, 010, 2);
+
+insert into Stored values
+
+	(53, 57008, 024, 1);
+
+insert into Stored values
+
+	(54, 57008, 027, 1);
+
+insert into Stored values
+
+	(55, 57008, 011, 2);
+
+insert into Stored values
+
+	(56, 57008, 006, 1);
+
+insert into Stored values
+
+	(57, 57008, 028, 1);
+
+insert into Stored values
+
+	(58, 57008, 005, 3);
+
+insert into Stored values
+
+	(59, 57009, 029, 2);
+
+insert into Stored values
+
+	(60, 57009, 030, 4);
+
+insert into Stored values
+
+	(61, 57009, 031, 1);
+
+insert into Stored values
+
+	(62, 57009, 032, 15);
+
+insert into Stored values
+
+	(63, 57009, 034, 1);
+
+insert into Stored values
+
+	(64, 57009, 035, 5);
+
+insert into Stored values
+
+	(65, 57009, 036, 7);
+
+insert into Stored values
+
+	(66, 57009, 037, 7);
+
+insert into Stored values
+
+	(67, 57009, 015, 3);
+
+insert into Stored values
+
+	(68, 57009, 038, 3);
+
+insert into Stored values
+
+	(69, 57009, 039, 4);
+
+insert into Stored values
+
+	(70, 57009, 040, 2);
+
+insert into Stored values
+
+	(71, 57009, 041, 4);
+
+insert into Stored values
+
+	(72, 57010, 042, 6);
+
+insert into Stored values
+
+	(73, 57010, 043, 4);
+
+insert into Stored values
+
+	(74, 57010, 032, 10);
+
+insert into Stored values
+
+	(75, 57010, 044, 2);
+
+insert into Stored values
+
+	(76, 57010, 038, 1);
+
+insert into Stored values
+
+	(77, 57010, 029, 9);
+
+insert into Stored values
+
+	(78, 57010, 031, 5);
+
+insert into Stored values
+
+	(79, 57010, 036, 12);
+
+insert into Stored values
+
+	(80, 57010, 035, 5);
+
+insert into Stored values
+
+	(81, 57010, 045, 6);
+
+insert into Stored values
+
+	(82, 57010, 046, 6);
+
+insert into Stored values
+
+	(83, 57010, 034, 3);
+
+insert into Stored values
+
+	(84, 57010, 15, 5);
